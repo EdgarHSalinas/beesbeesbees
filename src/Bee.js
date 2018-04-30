0abb1e85d62b691c35d49609b809c5ba4b5cac86
Edgar Salinas <EdgarHSalinas@gmail.com>
2018-04-30 13:06:18 -0700

    passed all tests

--- src/Bee.js
+++ src/Bee.js
@@ -2,7 +2,8 @@ var Bee = function() {
   Grub.call(this);
   this.age = 5;
   this.color = 'yellow';
-  // this.food = 
+  this.job = 'keep on growing';
+  
 };
 
 Bee.prototype = Object.create(Grub.prototype);
